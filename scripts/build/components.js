bbd6001836016416831f1485411f8ff28b334e51
firebushtree <firebushtree@163.com>
2024-06-08 23:02:34 +0800

    chore: remove useless ts include

--- scripts/build/components.js
+++ scripts/build/components.js
@@ -2,7 +2,7 @@ import path from 'node:path'
 import { build, defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Dts from 'vite-plugin-dts'
-import { componentsPath } from './path.js'
+import { componentsPath, tsconfigPath } from './path.js'
 
 const outputDir = path.resolve(componentsPath, './dist/es')
 
@@ -31,6 +31,7 @@ export function buildComponents() {
         root: componentsPath,
         entryRoot: path.resolve(componentsPath, './'),
         outDir: path.resolve(outputDir, './types'),
+        tsconfigPath,
       }),
     ],
   })
--- scripts/build/path.js
+++ scripts/build/path.js
@@ -1,5 +1,6 @@
 import path from 'node:path'
 
 export const __dirname = path.resolve()
+export const tsconfigPath = path.resolve(__dirname, './tsconfig.json')
 export const packagesPath = path.resolve(__dirname, './packages')
 export const componentsPath = path.resolve(packagesPath, './components')
