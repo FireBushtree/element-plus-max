82c15e26283fe521911256aaaae3d87351265e9c
firebushtree <firebushtree@163.com>
2024-06-12 16:04:29 +0800

    feat: init some list page props

--- packages/components/list-page/src/helper.ts
+++ packages/components/list-page/src/helper.ts
@@ -2,4 +2,13 @@ import type { ElMaxListPageProps } from './types'
 
 export const elMaxListPageDefaultProps: ElMaxListPageProps = {
   hasSearchForm: true,
+  searchForm: [],
+  hasNew: true,
+  hasEdit: true,
+  hasView: true,
+  hasDelete: true,
+  newText: '新增',
+  editText: '修改',
+  viewText: '查看',
+  deleteText: '删除',
 }
--- packages/components/list-page/src/types.ts
+++ packages/components/list-page/src/types.ts
@@ -1,3 +1,39 @@
+import type { ElMaxFormContent } from '@element-plus-max/components'
+
 export interface ElMaxListPageProps {
-  hasSearchForm?: boolean
+  hasSearchForm: boolean
+  searchForm: ElMaxFormContent
+  /**
+   * 是否有新增按钮
+   */
+  hasNew: boolean
+  /**
+   * 是否有编辑按钮
+   */
+  hasEdit: boolean
+  /**
+   * 是否有查看按钮
+   */
+  hasView: boolean
+  /**
+   * 是否有删除按钮
+   */
+  hasDelete: boolean
+
+  /**
+   * 新增按钮文案
+   */
+  newText: string
+  /**
+   * 修改按钮文案
+   */
+  editText: string
+  /**
+   * 查看按钮文案
+   */
+  viewText: string
+  /**
+   * 删除按钮文案
+   */
+  deleteText: string
 }
